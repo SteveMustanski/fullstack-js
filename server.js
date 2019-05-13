946998159ff4bed8a2b8aea13264b982fafbbfd2
SteveMustanski <steve.mustanski@gmail.com>
2019-05-13 13:12:39 -0400

    added plumbing for front end

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import config from './config';
+import apiRouter from './api';
+
+const server = express();
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('index');
+});
+
+server.listen(config.port, () => {
+  console.info(`Express listening on port ${config.port}`);
+});
