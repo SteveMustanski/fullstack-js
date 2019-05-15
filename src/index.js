15c8bbc9eeba7c4998ff175077d79bf3a4786613
SteveMustanski <steve.mustanski@gmail.com>
2019-05-15 17:50:27 -0400

    added proptypes using newest package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const App = props => {
   return <h2 className='text-center'>{props.headerMessage}</h2>;
+};
+
+App.propTypes = {
+  headerMessage: PropTypes.string,
 };
 
 ReactDOM.render(
