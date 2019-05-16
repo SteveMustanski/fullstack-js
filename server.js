659704d727bc37d9fbf56c73175302317fccc227
SteveMustanski <steve.mustanski@gmail.com>
2019-05-16 13:15:15 -0400

    added styles to style the contestpreview contents

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 import config from './config';
 import apiRouter from './api';
@@ -7,6 +9,12 @@ const server = express();
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
+server.use(
+  sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public'),
+  }),
+);
 
 server.set('view engine', 'ejs');
 
